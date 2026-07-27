2450c1b583fa26dcd0fa8191ba93423408f55f59
Altamirando-ti <altamirando.ti@gmail.com>
2026-07-26 22:29:19 -0300

    Aula 21. Criando, conhecendo e entendendo apps do Django

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Página raiz')
+
+def sobre(request):
+    return HttpResponse('Retornando uma string')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', sobre),
+    path('', home)
 ]
